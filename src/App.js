d2c18dd18b6d7fbaedf7b61edbbcf69fbd96d324
Peter <Posoo617@gmail.com>
2020-09-09 16:41:32 +0300

    Navbar (React Icons) and useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react"
 import "./App.css"
-import Home from "./components/Home"
-import Rooms from "./components/Rooms"
-import SingleRoom from "./components/SingleRoom"
-import Error from "./components/Error"
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+import Error from "./pages/Error"
 
 import { Route, Switch } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 const App = () => {
 	return (
 		<>
+			<Navbar />
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/rooms" component={Rooms} />
