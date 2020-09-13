33e5a7b98347b64772fdb3d5c69b202a89996646
Peter <Posoo617@gmail.com>
2020-09-13 13:28:25 +0300

    Context

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+import { RoomContext } from "../context"
+
+class FeaturedRooms extends Component {
+	static contextType = RoomContext
+	render() {
+		const value = this.context
+		console.log(value)
+		return <div>hello from featured rooms {value} </div>
+	}
+}
+
+export default FeaturedRooms
